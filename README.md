795e0f583bfbc01a50cc418062a95c49ca7abcbb
viniciusban <viniciusban@gmail.com>
2015-04-01 03:25:28 -0300

    Added README.md and clog.sh

---
+++ README.md
@@ -0,0 +1,50 @@
+clog.sh
+=======
+
+Generate CHANGELOG from your git history.
+
+```
+$ cd ~/projects/my_repo
+$ clog.sh --title v1.5
+v1.5
+  - #690: Protect tables from delete cascade. (2777fe4 by viniciusban)
+  - #695: Control commits by hand. (c1cacb9 by john.doe)
+  - #875: Correct source encoding. (478015d by mrs.smith)
+```
+
+You can type `--from` and `--to` arguments. Ask for help:
+
+```
+$ clog.sh --help
+```
+
+To update your CHANGELOG file:
+
+```
+$ clog.sh --title v1.5 | cat - CHANGELOG > CHANGELOG
+```
+
+
+Why?
+----
+
+See the motivation: [clog - A conventional changelog generator for the rest of us](http://blog.thoughtram.io/announcements/tools/2014/09/18/announcing-clog-a-conventional-changelog-generator-for-the-rest-of-us.html).
+
+
+Differences
+-----------
+
+- Simple and plain git, bash and awk. No external dependencies.
+- Simpler commit message standard. We just for "closes #" string.
+
+Tips
+----
+
+- Close only one ticket by line.
+- Write your closing ticket line as you want to see in changelog.
+- You can write your "close command" anywhere in your commit message.
+
+Collaborate
+-----------
+
+You are welcome. Fork and send a pull request.
---
+++ clog.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+
+# Generate a changelog based on git history.
+
+##--------------------------------------------------
+## parse and validate args.
+##--------------------------------------------------
+
+function _main () {
+    FROM=$(git tag --sort=refname | tail -n 1)
+    TO="HEAD"
+    TITLE="since $FROM"
+
+    while [ -n "$1" ]; do
+        case "$1" in
+            ( -f | --from ) FROM="$2"; shift ;;
+            ( -t | --to ) TO="$2"; shift ;;
+            ( -i | --title ) TITLE="$2"; shift ;;
+            ( -h | --help ) _show_help; exit 0 ;;
+            ( * ) echo "invalid option: $1"; exit 1 ;;
+        esac
+        shift
+    done
+
+##--------------------------------------------------
+## prepare for running
+##--------------------------------------------------
+
+    rm -f ${TMPDIR}/_clog*
+    echo "${TITLE}" > ${TMPDIR}/_clog_title.txt
+
+
+##--------------------------------------------------
+## showtime!
+##--------------------------------------------------
+
+    git log --reverse --grep='[cC]loses #' ${FROM}..${TO} | awk '\
+$1 == "commit" { sha1=$2 }
+$1 == "Author:" { author=$NF }
+$0 ~ /[cC]loses #/ {
+    line=$0
+    sub(/^.*[cC]loses #/, "#", line)
+    sub(/@.*/, "", author)
+    printf("  - %s (%s by %s)\n", line, substr(sha1,1,7), substr(author,2))
+}
+' > ${TMPDIR}/_clog_changelog.txt
+
+    cat ${TMPDIR}/_clog_changelog.txt | awk '{print $2}' | sort -u > ${TMPDIR}/_clog_unique_tickets.txt
+    if [ "$(wc -l ${TMPDIR}/_clog_changelog.txt | awk '{print $1}')" != "$(wc -l ${TMPDIR}/_clog_unique_tickets.txt | awk '{print $1}')" ]; then
+        echo "** Warning: Check the generated changelog. You possibly closed the same ticket twice." >&2
+    fi
+
+    echo "" > ${TMPDIR}/_clog_empty_line.txt
+    cat ${TMPDIR}/_clog_title.txt ${TMPDIR}/_clog_changelog.txt ${TMPDIR}/_clog_empty_line.txt
+}
+
+
+function _show_help () {
+    echo "
+Usage: $(basename $0) <options>
+
+Generate changelog from your git history.
+
+Options:
+    -f | --from <initial revision>
+        Get commit messages from <initial revision>. It can be a commit SHA1 or a tag.
+        If omitted, assumes last tag in lexicographic order.
+
+    -t | --to <final revision>
+        Get commit messages until <final revision>. It can be a commit SHA1, a tag or HEAD.
+        Assumes "HEAD" if omitted.
+
+    -i | --title  <text>
+        Puts <text> as title.
+"
+}
+
+
+_main $@
